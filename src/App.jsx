dee1ea1cb9a931a578f9fec7ed22c762d4886937
= <=rnanziani@gmail.com>
2025-06-29 15:19:44 -0400

    se agregan los componentes Login y Register

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
-import Register from './components/Register'
-import Login from './components/Login'
+import Register from './components/Register/Register'
+import Login from './components/Login/Login'
 
 function App () {
   return (
